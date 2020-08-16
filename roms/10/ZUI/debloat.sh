37291ca7968ab6d780b066266dcc8307a0e7efa4
Ridhin Issac Abraham <62167807+123ridhin@users.noreply.github.com>
2020-08-16 10:24:59 +0300

    Update debloat.sh

--- roms/10/ZUI/debloat.sh
+++ roms/10/ZUI/debloat.sh
@@ -6,3 +6,31 @@ thispath=`cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd`
 rm -rf $1/preinstall
 rm -rf $1/priv-app/ZuiXlog
 rm -rf $1/priv-app/ZuiCamera
+rm -rf $1/priv-app/EmergencyInfo
+rm -rf $1/priv-app/LenovoStore
+rm -rf $1/priv-app/LeVoiceAgent
+rm -rf $1/priv-app/LeVoiceTrustAgent
+rm -rf $1/priv-app/ZuiBug2Go
+rm -rf $1/priv-app/ZuiCamera
+rm -rf $1/priv-app/ZuiDolphin
+rm -rf $1/priv-app/ZuiFileTransfer
+rm -rf $1/priv-app/ZuiGallery
+rm -rf $1/priv-app/ZuiUHealth
+rm -rf $1/media/wallpapers
+rm -rf $1/media/audio/alarms
+rm -rf $1/app/EasterEgg
+rm -rf $1/app/LiveWallpapersPicker
+rm -rf $1/app/PrintSpooler
+rm -rf $1/app/talkback
+rm -rf $1/app/WallpaperBackup
+rm -rf $1/app/ZRoaming
+rm -rf $1/app/ZuiAlarm
+rm -rf $1/app/ZuiWeather
+rm -rf $1/themes/avengers
+rm -rf $1/themes/golden
+rm -rf $1/themes/grace
+rm -rf $1/themes/pink
+rm -rf $1/themes/superhero
+#endofline
+
+
